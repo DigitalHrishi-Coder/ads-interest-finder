61c7321edca34614420e568a9a4971d50a7c0336
DigitalHrishi-Coder <hrishikhandate@gmail.com>
2025-04-12 00:12:00 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AdsInterestFinder from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AdsInterestFinder />
+  </React.StrictMode>
+)
